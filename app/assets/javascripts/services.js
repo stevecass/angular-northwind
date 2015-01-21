f57302c6bd364c09d7af6f2ad0ad52271def543e
Steven Cassidy <steven@mumsnet.com>
2015-01-20 21:16:17 -0500

    Implement a general finderService

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -6,3 +6,16 @@ northwind.factory('Order', function($resource) {   return $resource('/api/orders
 northwind.factory('Product', function($resource) {   return $resource('/api/products/:id');  });
 northwind.factory('Shipper', function($resource) {   return $resource('/api/shippers/:id');  });
 northwind.factory('Supplier', function($resource) {   return $resource('/api/suppliers/:id');  });
+
+northwind.factory('finderService', ['$http', function($http){
+  var service = {
+    search: function(what, field, val, exact) {
+    var s = arguments.join("/");
+    var promise = $http.get(s).then(function(response){
+      return response.data;
+    });
+    return promise;
+    }
+  };
+  return service;
+}]);
--- app/assets/javascripts/welcome-controllers.js
+++ app/assets/javascripts/welcome-controllers.js
@@ -1,23 +1,29 @@
 
-northwind.controller('MainController', ['$scope', '$filter', 'Category', 'Product', function($scope, $filter, Category, Product){
-   $scope.data = {};
+northwind.controller('MainController', ['$scope', '$filter', 'Category', 'Product', 'finderService', function($scope, $filter, Category, Product, finderService){
+  $scope.data = {};
 
-   Category.query(function(data){
+  Category.query(function(data){
     $scope.data.categories = data
-   });
+  });
 
-   Product.query(function(data){
+  Product.query(function(data){
     $scope.data.products = data
-   });
+  });
 
-   $scope.setCurrentCat = function(item) {
+  $scope.setCurrentCat = function(item) {
     $scope.current_cat = item;
-   }
+  }
 
-   $scope.stockLevelClass = function(item) {
-      return item.units_in_stock < item.reorder_level ? "stock_level_danger" : "stock_level_ok";
+  $scope.stockLevelClass = function(item) {
+    return item.units_in_stock < item.reorder_level ? "stock_level_danger" : "stock_level_ok";
+  }
 
-   }
+  $scope.search = function(what, field, val, exact) {
+    finderService.search(what, field, val, exact).then(function(data){
+      $scope.search_results = data;
+    });
+  }
+
 
 }]);
 
