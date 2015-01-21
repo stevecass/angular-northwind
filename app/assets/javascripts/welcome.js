569e0ef1ffd22b3635bd7233355598972c514572
Steven Cassidy <steven@mumsnet.com>
2015-01-20 19:46:18 -0500

    Define a module and controller

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var northwind = angular.module('northwind', []);
+
+northwind.controller('MainController', ['$scope', function($scope){
+   $scope.data = {};
+   $scope.data.items = ['apple', 'banana', 'cherry'];
+}]);
