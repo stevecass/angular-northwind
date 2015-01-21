9dec36a88b0a4c6ad60ee7303380a63b96fc364c
Steven Cassidy <steven@mumsnet.com>
2015-01-20 20:00:27 -0500

    Use angular $resource to retrieve categories

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var northwind = angular.module('northwind', []);
+var northwind = angular.module('northwind', ['ngResource']);
 
-northwind.controller('MainController', ['$scope', function($scope){
+northwind.factory('Category', function($resource) {
+  return $resource('/api/categories/:id'); // Note the full endpoint address
+});
+
+northwind.controller('MainController', ['$scope', 'Category', function($scope, Category){
    $scope.data = {};
-   $scope.data.items = ['apple', 'banana', 'cherry'];
+
+   Category.query(function(data){
+    $scope.data.categories = data
+   });
 }]);
+
+
+
