32b30c9766fa8b2fb4fd95cf69f2bf635669ad14
stevecass <steven@mumsnet.com>
2015-03-05 11:11:48 -0500

    Add empty / non-empty class methods to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ActiveRecord::Base
 
   has_many :products
 
+  def self.empty
+    Category.includes(:products).where(:products => { :id => nil })
+  end
+
+  def self.non_empty
+    Category.includes(:products)
+  end
+
 end
